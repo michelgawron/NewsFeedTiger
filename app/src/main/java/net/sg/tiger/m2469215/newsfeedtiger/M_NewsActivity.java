bd50c22a1f3af0b1d843f20f6a4e24b6b22eb3b4
Nicolas Michel <nico.michmich@sfr.fr>
2017-05-30 21:43:41 +0200

    Adding functionnalities to our app
    Adding mongodb gradle dependencies

--- app/src/main/java/net/sg/tiger/m2469215/newsfeedtiger/M_NewsActivity.java
+++ app/src/main/java/net/sg/tiger/m2469215/newsfeedtiger/M_NewsActivity.java
@@ -3,6 +3,19 @@ package net.sg.tiger.m2469215.newsfeedtiger;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.support.v4.app.Fragment;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.Block;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+
+import org.bson.Document;
+
 /**
  * Created by M2469215 on 30/05/2017.
  * This class purpose is to be used as a model to handle the data and transfer it to the activity
@@ -13,12 +26,49 @@ public class M_NewsActivity {
     /**
      * URL of our mongodb database
      */
-    private final static String URLMONGO = " ";
+    private final static String URLMONGO = "52.233.183.232";
+
+    /**
+     * MongoDB port
+     */
+    private final static int PORTMONGO = 27017;
+
+    /**
+     * MongoDB database name
+     */
+    private final static String DATABASENAME = "local";
+
+    /**
+     * MongoDB collection name
+     */
+    private final static String COLLECTIONNAME = "documents";
+
 
     /**
      * News feed - we are going to get it from MongoDB
      */
     private ArrayList<M_News> listNews;
+
+    /**
+     * Current articles loaded (0 at the beginning, loading 20 more each time)
+     */
+    private int currentLoad;
+
+    /**
+     *
+     * @return
+     */
+    public int getCurrentLoad() {
+        return currentLoad;
+    }
+
+    /**
+     *
+     * @param currentLoad
+     */
+    public void setCurrentLoad(int currentLoad) {
+        this.currentLoad = currentLoad;
+    }
 
     /**
      * Getter for the news list
@@ -34,6 +84,7 @@ public class M_NewsActivity {
      */
     public M_NewsActivity() {
         this.listNews = new ArrayList<M_News>();
+        this.currentLoad = 0;
     }
 
     /**
@@ -52,6 +103,19 @@ public class M_NewsActivity {
      */
     public void addNews(ArrayList<M_News> newList)
     {
+
+    }
+
+    /**
+     * Getting a list of news from mongodb
+     * @param start Index start
+     * @param end Index end
+     * @return A list of news to be displayed
+     */
+    public ArrayList<M_News> getFromMongo(int start, int end)
+    {
+        MongoClient client = new MongoClient(URLMONGO, PORTMONGO);
+        MongoCollection<Document> coll = client.getDatabase(DATABASENAME).getCollection(COLLECTIONNAME);
 
     }
 }
